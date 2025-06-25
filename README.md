b04358e58eaeb3ddf668cc2234e3bbe22e57d133
nishahariii <nishahariii@gmail.com>
2025-06-25 08:33:41 +0530

    data validation

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # TextSummarizer
 
 ##Workflows
-config.yaml
-params.yaml
-entity
+1. update config/config.yaml
+2. update params.yaml
+3. update src/entity/__init__.py
 src/config/configuration.py
 components
 pipeline
 main.py
 app.py
+
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # print (sys.path)
 
 from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -9,8 +10,17 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} has started. <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f">>>>>> Stage {STAGE_NAME} completed.\n\n <<<<<<")
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed. <<<<<<\n\n")
 except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} has started. <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed. <<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
